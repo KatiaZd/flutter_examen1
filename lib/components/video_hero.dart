c2d99b842b084a1425cbb6cbb61769160fb1d1be
Katia Zd <katia.zouad@hotmail.fr>
2023-12-04 15:47:35 +0100

    suppression bordures video

--- lib/components/video_hero.dart
+++ lib/components/video_hero.dart
@@ -11,22 +11,20 @@ class VideoHero extends StatefulWidget {
 class _VideoHeroState extends State<VideoHero> {
   late VideoPlayerController _videoPlayerController;
   late Future<void> _initializeVideoPlayerFuture;
+  
 
   @override
   void initState() {
     super.initState();
 
     _videoPlayerController = VideoPlayerController.asset(
-      'web/assets/video/v1.mp4',
-    )..initialize().then((_) {
-      setState(() {
-        // Force la reconstruction du widget après l'initialisation du contrôleur vidéo
-      });
-    });
-
+        'web/assets/video/v1.mp4',
+      );
     _initializeVideoPlayerFuture = _videoPlayerController.initialize();
+    _videoPlayerController.setLooping(true); // Optionnel : Boucler la vidéo
+    _videoPlayerController.play(); // Optionnel : Lancer automatiquement la vidéo
   }
-
+  
   @override
   void dispose() {
     _videoPlayerController.dispose();
@@ -35,31 +33,57 @@ class _VideoHeroState extends State<VideoHero> {
 
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
-    double screenHeight = MediaQuery.of(context).size.height;
-
+    
     return Scaffold(
-      appBar: AppBar(
+      
+      body: Stack(
+        children: [
+          FutureBuilder(
+            future: _initializeVideoPlayerFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return AspectRatio(
+                  aspectRatio: _videoPlayerController.value.aspectRatio,
+                  child: VideoPlayer(_videoPlayerController),
+                );
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
+          ),
+        const Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+            
+              children: [
+                Text(
+                  'France Data',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                Text(
+                  'Atlas des données de France',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-      body: FutureBuilder(
-        future: _initializeVideoPlayerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            _videoPlayerController.setVolume(1.0);
-            return AspectRatio(
-              aspectRatio: _videoPlayerController.value.aspectRatio,
-              child: VideoPlayer(_videoPlayerController),
-            );
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
-      ),
+        
       floatingActionButton: FloatingActionButton(
         onPressed: () {
+          
           setState(() {
+            
             if (_videoPlayerController.value.isPlaying) {
               _videoPlayerController.pause();
             } else {
@@ -67,13 +91,14 @@ class _VideoHeroState extends State<VideoHero> {
             }
           });
         },
-        child: Icon(
-          _videoPlayerController.value.isPlaying
-              ? Icons.pause
-              : Icons.play_arrow,
-        ),
-      ),
-    );
+        backgroundColor: const Color.fromARGB(255, 152, 191, 253),
+            child: Icon(
+              _videoPlayerController.value.isPlaying
+                  ? Icons.pause
+                  : Icons.play_arrow,
+              color: const Color.fromARGB(255, 254, 254, 254), // Couleur de l'icône
+            ),
+          ), 
+      );
   }
 }
-
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_examen1/components/menu.dart';
 import 'package:flutter_examen1/components/video_hero.dart';
 import 'package:flutter_examen1/pages/regions_page.dart';
+import 'package:flutter_examen1/components/menu.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key, required this.title}) : super(key: key);
@@ -12,51 +12,53 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          title, 
+          title,
           style: const TextStyle(
             color: Colors.white,
           ),
         ),
-        backgroundColor: Theme.of(context).colorScheme.primary,
+        backgroundColor: Theme.of(context).primaryColor,
         iconTheme: const IconThemeData(color: Colors.white),
       ),
       backgroundColor: Colors.white,
-      drawer: AppDrawer(),
-      body: Column(
-        children: <Widget>[
+      drawer: const AppDrawer(),
+      body: Stack(
+        children: <Widget> [
           Container(
-            padding: const EdgeInsets.all(16.0),                    
-            ),
-          
-          Expanded(
-            child: Stack(
-              children: [
-                VideoHero(),
-                Center(
-                  child: Text(
-                    'France Data Atlas des données de France', 
-                    style: Theme.of(context).textTheme.headline5!.copyWith(color: Colors.white),
+        margin: EdgeInsets.zero, // Définir la marge à zéro
+        child: const VideoHero(),
+      ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(height: 20),
+                Container(
+                  height: 550, // Définissez la hauteur souhaitée
+                  child: const SizedBox
+                      .shrink(), // Ajoutez d'autres composants ici, si nécessaire
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              const RegionPage(title: "Region Page")),
+                    );
+                  },
+                  style: ButtonStyle(
+                    fixedSize: MaterialStateProperty.all(const Size(250, 40)),
+                  ),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.search, color: Colors.blueAccent),
+                      SizedBox(width: 5),
+                      Text("Rechercher Par Région", style: TextStyle(color: Colors.blueAccent)),
+                  ],
                   ),
                 ),
-              ],
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const RegionPage(title: "Region Page")),
-              );
-            },
-            style: ButtonStyle(
-              fixedSize: MaterialStateProperty.all(Size(250, 40)),
-            ),
-            child: const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.search),
-                SizedBox(width: 5),
-                Text("Rechercher Par Région"),
               ],
             ),
           ),
